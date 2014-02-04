8941f9516f70d55434e336149790a502e56cf36b
vincentjzeng <vincentjzeng@gmail.com>
2014-02-04 22:34:23 +0800

    Init commit

---
+++ src/main/java/com/vincentjzeng/petsalvation/message/req/TextMessage.java
@@ -0,0 +1,18 @@
+package com.vincentjzeng.petsalvation.message.req;
+
+public class TextMessage extends RequestMessage {
+
+	// ÏûÏ¢ÄÚÈİ  
+    private String Content;
+
+	public String getContent() {
+		return Content;
+	}
+
+	public void setContent(String content) {
+		Content = content;
+	} 
+    
+    
+	
+}
